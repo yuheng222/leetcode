f7ea76aa71365bb98fa814e9a68d442f9c2fa7a8
Xavier Xie <xavier.xyh@gmail.com>
2022-07-12 11:06:51 +0800

    Time: 151 ms (35.18%), Space: 19.5 MB (30.72%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return res.values()
