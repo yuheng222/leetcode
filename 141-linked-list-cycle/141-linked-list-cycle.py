61eb71c086ecdcbe9be38cd631ea309d3ca3885c
Xavier Xie <xavier.xyh@gmail.com>
2022-07-13 16:05:57 +0800

    Time: 52 ms (76.40%), Space: 20.3 MB (93.47%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -12,8 +12,7 @@ class Solution(object):
         """
         if not head:
             return False
-        slow = head
-        fast = head.next
+        slow, fast = head, head.next
         while slow != fast:
             if fast is None or fast.next is None:
                 return False
