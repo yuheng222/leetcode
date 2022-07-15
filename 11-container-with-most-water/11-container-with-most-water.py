55a75cdbe8ef5ff3a9a2f86c367bbc20156e7aa3
Xavier Xie <xavier.xyh@gmail.com>
2022-07-16 00:15:52 +0800

    Time: 821 ms (52.59%), Space: 24.1 MB (6.06%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -8,7 +8,7 @@ class Solution(object):
         max_amt = 0
         while l < r:
             max_amt = max(min(height[l], height[r]) * (r - l), max_amt)
-            if height[l] < height[r]:
+            if height[l] <= height[r]:
                 l += 1
             else:
                 r -= 1
