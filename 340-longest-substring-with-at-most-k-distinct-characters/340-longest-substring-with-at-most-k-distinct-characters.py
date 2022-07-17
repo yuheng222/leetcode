d6bcbb5a52c27d9fdebb37a16ec9ecad6e470f23
Xavier Xie <xavier.xyh@gmail.com>
2022-07-17 23:16:24 +0800

    Time: 145 ms (20.77%), Space: 15.3 MB (18.72%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.py
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.py
@@ -5,17 +5,16 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        char_count = {}
-        l = 0
+        count = {} # count of characters that is within the sliding window
         max_length = 0
+        l = 0
         for r in range(len(s)):
-            char_count[s[r]] = 1 + char_count.get(s[r], 0)
-            while len(char_count) > k:
-                char_count[s[l]] -= 1
-                if char_count[s[l]] == 0:
-                    del char_count[s[l]]
+            count[s[r]] = 1 + count.get(s[r], 0)
+            while len(count) > k:
+                count[s[l]] -= 1
+                if count[s[l]] == 0:
+                    del count[s[l]]
                 l += 1
             max_length = max(max_length, r - l + 1)
         return max_length
-        
             
