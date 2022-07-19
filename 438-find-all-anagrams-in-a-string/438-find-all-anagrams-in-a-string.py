f4f5db4db42dfd1c6e170cfeb19c6664c56243f2
Xavier Xie <xavier.xyh@gmail.com>
2022-07-19 23:18:41 +0800

    Time: 189 ms (40.62%), Space: 13.9 MB (98.60%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -6,7 +6,7 @@ class Solution(object):
         :rtype: List[int]
         """
         pattern_counter = collections.Counter(p)
-        window_counter = collections.Counter(s[:len(p)])
+        window_counter =  collections.Counter(s[:len(p)])
         res = []
         
         if window_counter == pattern_counter:
@@ -14,8 +14,8 @@ class Solution(object):
         
         window_start = 0
         
-        for window_end, value in enumerate(s[len(p):]):
-            window_counter[value] += 1
+        for window_end, letter in enumerate(s[len(p):]):
+            window_counter[letter] += 1
             window_start_char = s[window_start]
             window_counter[window_start_char] -= 1
             if window_counter[window_start_char] == 0:
@@ -23,5 +23,7 @@ class Solution(object):
             window_start += 1
             if window_counter == pattern_counter:
                 res.append(window_start)
+        
         return res
+            
             
