ab9167fbf001029d2db77d7ea791d5c4e5319082
Xavier Xie <xavier.xyh@gmail.com>
2022-07-19 23:07:07 +0800

    Time: 277 ms (19.89%), Space: 14.3 MB (70.40%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        pattern_counter = collections.Counter(p)
+        window_counter = collections.Counter(s[:len(p)])
+        res = []
+        
+        if window_counter == pattern_counter:
+            res.append(0)
+        
+        window_start = 0
+        
+        for window_end, value in enumerate(s[len(p):]):
+            window_counter[value] += 1
+            window_start_char = s[window_start]
+            window_counter[window_start_char] -= 1
+            if window_counter[window_start_char] == 0:
+                del window_counter[window_start_char]
+            window_start += 1
+            if window_counter == pattern_counter:
+                res.append(window_start)
+        return res
+            
