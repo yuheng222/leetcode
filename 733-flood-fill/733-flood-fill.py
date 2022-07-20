82603b4e35683a519a3d92588dd89c3f4131486e
Xavier Xie <xavier.xyh@gmail.com>
2022-07-21 01:10:54 +0800

    Time: 95 ms (38.20%), Space: 13.3 MB (98.63%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def dfs(self, image, x, y, oldColor, newColor):
+        if x < 0 or x >= len(image) or y < 0 or y >= len(image[0]):
+            return # if out of bounds, return
+        if image[x][y] != oldColor:
+            return # if cell is not the same as the original color, it doesnt have to be filled
+        
+        image[x][y] = newColor
+        self.dfs(image, x + 1, y, oldColor, newColor)
+        self.dfs(image, x - 1, y, oldColor, newColor)
+        self.dfs(image, x, y + 1, oldColor, newColor)
+        self.dfs(image, x, y - 1, oldColor, newColor)
+            
+    def floodFill(self, image, sr, sc, color):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type color: int
+        :rtype: List[List[int]]
+        """
+        if image[sr][sc] != color:
+            self.dfs(image, sr, sc, image[sr][sc], color)
+        return image
+            
+    
+        
