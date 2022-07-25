8180b017b6c781dbd66ffb54f82d831550575887
Xavier Xie <xavier.xyh@gmail.com>
2022-07-26 01:33:49 +0800

    Time: 64 ms (60.55%), Space: 15.5 MB (88.41%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -9,18 +9,13 @@ class Solution:
         def find_paths(currentNode, targetSum, currentPath, allPaths):
             if not currentNode:
                 return None
-            # add current node to path
             currentPath.append(currentNode.val)
-            # if current node is leaf and its value is equal to targetSum, save the current path
-            if currentNode.val == targetSum and not currentNode.left and not currentNode.right:
+            if currentNode.val == targetSum and currentNode.left is None and currentNode.right is None:
                 allPaths.append(list(currentPath))
             else:
                 find_paths(currentNode.left, targetSum - currentNode.val, currentPath, allPaths)
                 find_paths(currentNode.right, targetSum - currentNode.val, currentPath, allPaths)
-            
-            # remove the current node from path to backtrack
-            # the current node needs to be removed while going up the recursive call stack
-            del currentPath[-1]
-        all_paths = []
-        find_paths(root, targetSum, [], all_paths)
-        return all_paths
+            currentPath.pop()
+        allPaths = []
+        find_paths(root, targetSum, [], allPaths)
+        return allPaths
