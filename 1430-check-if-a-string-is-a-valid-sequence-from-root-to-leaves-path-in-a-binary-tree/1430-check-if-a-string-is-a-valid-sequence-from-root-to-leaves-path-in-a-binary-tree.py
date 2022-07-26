c9ec1d39e3de0e115c51d32b678115bbd682a352
Xavier Xie <xavier.xyh@gmail.com>
2022-07-26 11:25:47 +0800

    Time: 214 ms (23.60%), Space: 15.7 MB (36.65%) - LeetHub

--- 1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.py
+++ 1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/1430-check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.py
@@ -6,17 +6,12 @@
 #         self.right = right
 class Solution:
     def isValidSequence(self, root: Optional[TreeNode], arr: List[int]) -> bool:
-        def dfs(root, arr, arr_idx):
+        def dfs(root, arr, node_idx):
             if not root:
                 return False
-            arr_len = len(arr)
-            if arr_idx > arr_len - 1 or root.val != arr[arr_idx]:
+            if node_idx > len(arr) - 1 or root.val != arr[node_idx]:
                 return False
-            # if current node is a leaf, and it is at the end of the seq, then a path is found
-            if not root.left and not root.right and arr_idx == arr_len - 1:
+            if not root.left and not root.right and node_idx == len(arr) - 1:
                 return True
-            return dfs(root.left, arr, arr_idx + 1) or dfs(root.right, arr, arr_idx + 1)
-        if not root:
-            return len(arr) == 0
+            return dfs(root.left, arr, node_idx + 1) or dfs(root.right, arr, node_idx + 1)
         return dfs(root, arr, 0)
-            
