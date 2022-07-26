0a439cf7e46745e95edc091a10b0890bbe71876b
Xavier Xie <xavier.xyh@gmail.com>
2022-07-26 22:31:36 +0800

    Time: 135 ms (49.01%), Space: 21.3 MB (62.22%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -13,7 +13,8 @@ class Solution:
                 return 0
             left_path = max(dfs(root.left), 0)
             right_path = max(dfs(root.right), 0)
-            max_sum = max(max_sum, left_path +  right_path + root.val)
+            current_max_path = left_path +  right_path + root.val
+            max_sum = max(max_sum, current_max_path)
             return max(left_path, right_path) + root.val
         dfs(root)
         return max_sum
