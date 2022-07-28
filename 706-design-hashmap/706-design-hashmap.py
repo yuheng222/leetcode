01138a1d01a5c3d450334291cf6f955c63cb856e
Xavier Xie <xavier.xyh@gmail.com>
2022-07-28 21:01:22 +0800

    Time: 286 ms (80.94%), Space: 17.3 MB (53.49%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.py
@@ -0,0 +1,45 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 2069
+        self.buckets = [[] for _ in range(self.size)]
+        
+    def put(self, key: int, value: int) -> None:
+        bucket, i = self.getBucketIndex(key)
+        if i < 0:
+            bucket.append((key, value))
+        else:
+            bucket[i] = (key, value)
+
+    def get(self, key: int) -> int:
+        bucket, i = self.getBucketIndex(key)
+        if i < 0:
+            return -1
+        else:
+            return bucket[i][1]
+
+    def remove(self, key: int) -> None:
+        bucket, i = self.getBucketIndex(key)
+        if i < 0:
+            return
+        else:
+            bucket.remove(bucket[i])
+        
+    def getBucketNum(self, key):
+        return key % self.size
+    
+    def getBucketIndex(self, key):
+        bucketNum = self.getBucketNum(key)
+        bucket = self.buckets[bucketNum]
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                return bucket, i
+        return bucket, - 1
+    
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
