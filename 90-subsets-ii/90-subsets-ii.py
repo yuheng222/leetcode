b3b61d5228367d5803ee1bd74119e82fc9c7a9f6
Xavier Xie <xavier.xyh@gmail.com>
2022-07-29 00:26:01 +0800

    Time: 57 ms (54.88%), Space: 14.2 MB (9.47%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        subsets = []
+        subsets.append([])
+        end = 0
+        for i in range(len(nums)):
+            start = 0
+            # if current and previous elements are the same, create new subsets only from the subsets add in the prev step
+            if i > 0 and nums[i] == nums[i - 1]:
+                start = end + 1
+            end = len(subsets) - 1
+            for j in range(start, end+1):
+                new_set = list(subsets[j])
+                new_set.append(nums[i])
+                subsets.append(new_set)
+        return subsets
+            
