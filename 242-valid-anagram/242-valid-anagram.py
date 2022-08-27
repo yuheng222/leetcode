148452df7cef56ff6bc86a062c31b3381f9fafb1
Xavier Xie <xavier.xyh@gmail.com>
2022-08-27 18:24:42 +0800

    Time: 63 ms (72.79%), Space: 14.5 MB (67.20%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,17 +1,8 @@
-from collections import defaultdict
+from collections import Counter
 
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        s_dict = defaultdict(int)
-        t_dict = defaultdict(int)
-        if len(s) != len(t):
-            return False
-        for i in range(len(s)) :
-            s_dict[s[i]] += 1
-            t_dict[t[i]] += 1
-        return s_dict == t_dict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_count = Counter(s)
+        t_count = Counter(t)
+        return s_count == t_count
+        
