712bfd00c753be0f95ebceb8ab9a6c0ec5d03170
Xavier Xie <xavier.xyh@gmail.com>
2022-08-27 18:21:06 +0800

    Time: 470 ms (94.37%), Space: 25.9 MB (92.60%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,8 +1,11 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        return len(set(nums)) != len(nums)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = {}
+        for num in nums:
+            if num in seen:
+                return True
+            else:
+                seen[num] = 1
+        return False
+        
         
