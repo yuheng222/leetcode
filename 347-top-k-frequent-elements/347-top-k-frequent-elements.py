b3786ecc252762921bc602eaae418c17cdc39ef3
Xavier Xie <xavier.xyh@gmail.com>
2022-08-29 23:42:56 +0800

    Time: 166 ms (50.40%), Space: 18.6 MB (71.67%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -2,9 +2,9 @@ from heapq import *
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        res = []
         heap = []
         count = {}
-        res = []
         for num in nums:
             count[num] = 1 + count.get(num, 0)
         for num, freq in count.items():
@@ -14,4 +14,3 @@ class Solution:
         while heap:
             res.append(heappop(heap)[1])
         return res
-        
