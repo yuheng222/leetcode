b313d9ea27b7b4a1d9523dc6cca054536d0850c0
Xavier Xie <xavier.xyh@gmail.com>
2022-09-01 22:19:28 +0800

    Time: 170 ms (74.45%), Space: 13.9 MB (57.42%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,20 +1,13 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        l, longest, max_repeat = 0, 0, 0
-        char_count = {}
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        l = 0
+        res = 0
         for r in range(len(s)):
-            right_char = s[r]
-            char_count[right_char] = 1 + char_count.get(right_char, 0)
-            max_repeat = max(max_repeat, char_count[right_char])
-            # if more than k, window is shifted to the right without extending the window
-            if r - l + 1 - max_repeat > k:
-                left_char = s[l]
-                char_count[s[l]] -= 1
+            count[s[r]] = 1 + count.get(s[r], 0)
+            if r - l + 1 - max(count.values()) > k:
+                count[s[l]] -= 1
                 l += 1
-            longest = max(longest, r - l + 1)
-        return longest
+            res = max(res, r - l + 1)
+        return res
+        
