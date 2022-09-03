95c3b50740446288aa3d821b50bc9a741835a1a2
Xavier Xie <xavier.xyh@gmail.com>
2022-09-03 14:25:33 +0800

    Time: 5451 ms (5.01%), Space: 49.2 MB (5.11%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,28 +1,20 @@
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
         s_count, t_count = collections.Counter(), collections.Counter(t)
+        res = []
         l, r = 0, 0
-        results = []
-        # Expand window to try and match the characters in s with t
         while r < len(s):
             s_count[s[r]] += 1
             r += 1
-            # if the characters in the window doesn't match, continue expanding the window
             if s_count & t_count != t_count:
                 continue
             while l < r:
                 s_count[s[l]] -= 1
                 l += 1
-                # if window still matches the pattern, shrink the window
                 if s_count & t_count == t_count:
                     continue
-                results.append(s[l-1:r])
+                res.append(s[l-1:r])
                 break
-        if not results:
+        if not res:
             return ""
-        return min(results, key=len)
+        return min(res, key=len)
