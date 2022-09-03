6a2c447d03274189b05b1f55fd13e7abc27f7c12
Xavier Xie <xavier.xyh@gmail.com>
2022-09-03 14:40:35 +0800

    Time: 147 ms (65.79%), Space: 14.8 MB (36.52%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,20 +1,30 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        s_count, t_count = collections.Counter(), collections.Counter(t)
-        res = []
-        l, r = 0, 0
-        while r < len(s):
-            s_count[s[r]] += 1
-            r += 1
-            if s_count & t_count != t_count:
-                continue
-            while l < r:
-                s_count[s[l]] -= 1
-                l += 1
-                if s_count & t_count == t_count:
-                    continue
-                res.append(s[l-1:r])
-                break
-        if not res:
+        if t == "":
             return ""
-        return min(res, key=len)
+        s_count, t_count = {}, {}
+        for char in t:
+            t_count[char] = 1 + t_count.get(char, 0)
+        res, min_len = [-1, -1], float("inf")
+        have, need = 0, len(t_count)
+        l = 0
+        for r in range(len(s)):
+            char = s[r]
+            s_count[char] = 1 + s_count.get(char, 0)
+            if char in t_count and s_count[char] == t_count[char]:
+                have += 1
+            while have == need:
+                # update result
+                if r - l + 1 < min_len:
+                    res = [l, r]
+                    min_len = r - l + 1
+                # pop from the left of our window
+                s_count[s[l]] -= 1
+                # if the popped char was in t_count and now that freq of char in s_count is 0, decrement have
+                if s[l] in t_count and s_count[s[l]] < t_count[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l:r+1]
+                
+        
