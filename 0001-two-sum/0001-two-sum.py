b5a870467c1e3124a66029f18c1784f690e7507d
Xavier Xie <xavier.xyh@gmail.com>
2022-12-03 15:43:35 +0800

    Time: 62 ms (94.93%), Space: 15.5 MB (9.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         complement_map = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if nums[i] not in complement_map:
+        for i, num in enumerate(nums):
+            complement = target - num
+            if num not in complement_map:
                 complement_map[complement] = i
             else:
-                return [complement_map[nums[i]], i]
+                return [complement_map[num], i]
         return []
         
