0642d0a6b83a27ef14e52da3f5d95d3f09592b1d
Xavier Xie <xavier.xyh@gmail.com>
2022-12-11 02:01:51 +0800

    Time: 243 ms (16.17%), Space: 24 MB (38.26%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head:
+            return None
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+        
+        first, second = head, prev
+        while second.next:
+            nxt = first.next
+            first.next = second
+            first = nxt
+            nxt = second.next
+            second.next = first
+            second = nxt
+            
+            
