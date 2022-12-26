a7aa0ab699f3d035ce3a536db1ab9768d1ff4796
Xavier Xie <xavier.xyh@gmail.com>
2022-12-27 01:36:13 +0800

    Time: 43 ms (87.58%), Space: 14 MB (58.89%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -4,11 +4,12 @@ class Solution:
         if len(nums) == 1:
             return [nums[:]]
         for i in range(len(nums)):
-            num = nums.pop(0)
+            n = nums.pop(0)
             perms = self.permute(nums)
             for perm in perms:
-                perm.append(num)
-            nums.append(num)
+                perm.append(n)
+            # add the result for the current permutation
             res.extend(perms)
+            # backtrack, add back the num after removing it
+            nums.append(n)
         return res
-        
